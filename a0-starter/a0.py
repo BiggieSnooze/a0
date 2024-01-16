ea975b6717a08537db0412cede162cf10e74a898
Andrew <huynha15@uci.edu>
2024-01-15 19:29:27 -0800

    create and set i = 0 create for loop to start create # of boxes between 0 and user input if n(number of boxes to be created by user) > 1 and i == 1, creates the first box

--- a0-starter/a0.py
+++ a0-starter/a0.py
@@ -15,5 +15,12 @@ wall = "|"
 space = " "
 front = "  "
 
+i = 0
+
+for i in range(0, n + 1):
+    if n > 1 and i == 1:
+        print(f"{corner}{middle}{corner}")
+        print(f"{wall}{space}{wall}")
+        print(f"{corner}{middle}{corner}{middle}{corner}")
 
 
