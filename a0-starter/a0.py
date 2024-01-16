6e378afebaf75171a669eded4ed606c48ba42c6c
Andrew <huynha15@uci.edu>
2024-01-15 19:19:31 -0800

    Filled out name, email, and id

---
+++ a0-starter/a0.py
@@ -0,0 +1,11 @@
+# a0.py
+
+# Starter code for assignment 0 in ICS 32 Programming with Software Libraries in Python
+
+# Replace the following placeholders with your information.
+
+# Andrew Huynh
+# huynha15@uci.edu
+# 16106034
+
+
